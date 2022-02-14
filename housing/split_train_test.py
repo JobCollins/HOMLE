b7f0ec84a9dbde21bb96514f2a4ca82da943d685
JobCollins <dulo.collins@gmail.com>
2022-02-14 13:26:02 +0300

    Feature: Split Data by ID Since the above function use random slices to split data, running it a couple of times will ensure the ML algo wil see all of the data.
    To avoid this, compute a hash of each instance’s identifier and put that instance in the test set if the hash is lower or equal to 20% of the maximum hash value.
    
    This ensures that the test set will remain consistent across multiple runs, even if you refresh the dataset. The new test set will contain 20% of the new instances, but it will not contain any instance that was previously in the training set.

--- housing/split_train_test.py
+++ housing/split_train_test.py
@@ -1,4 +1,34 @@
 import numpy as np
+from zlib import crc32
+
+def test_set_check(identifier, test_ratio):
+    """
+    compute a hash of each instance’s identifier 
+    and put that instance in the test set 
+    if the hash is lower or equal to 20% of the maximum hash value.
+
+    input:
+              identifier: identifier column
+              test_ratio: percentage split of the data
+    output:
+              a test set check
+    """
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+     """
+    split data into train and test set by id
+    input:
+              data: data to split randomly into train and test sets
+              test_ratio: percentage split of the data
+              id_column: the unique identifier column
+    output:
+              data.loc[~in_test_set]: train set
+             data.loc[in_test_set]: test set
+    """
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
 
 def split_train_test(data, test_ratio):
     """
